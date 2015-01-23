1c9d88dfd55f2bf6f2a852b5a3e99b7d41415071
Charles Lehner <cel@celehner.com>
2015-01-23 16:31:21 -0500

    Update example results URL

--- config.example.php
+++ config.example.php
@@ -7,4 +7,4 @@ define('GITHUB_TOKEN', '0000000000000000000000000000000000000000');
 
 define('REPOS_DIR', './builds');
 define('RESULTS_DIR', './results');
-define('RESULTS_URL', 'http://example.org/results');
+define('RESULTS_URL', 'http://example.org/ci/results');
